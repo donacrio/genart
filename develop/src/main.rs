49016f33fcc589dfd342c801875e719ad908a01a
Donatien Criaud <donatien.criaud@gmail.com>
2022-12-01 15:00:00 +0100

    feat(utils): create pencil brush

--- develop/src/main.rs
+++ develop/src/main.rs
@@ -36,28 +36,33 @@ impl StaticArtwork for Model {
       .map(|i| {
         let h = (i as f64 / 5f64 - 0.5f64) * w_h as f64 / 2f64;
         let start: Coord = (-(w_w as f64) * 0.90f64 / 2f64, h).into();
-        let start = sample_coords(
-          start,
-          CoordType::Slant(0.01 * w_w as f64, 0.01 * w_h as f64),
-        );
         let end: Coord = (w_w as f64 * 0.90f64 / 2f64, h).into();
-        let end = sample_coords(end, CoordType::Slant(0.01 * w_w as f64, 0.01 * w_h as f64));
-
-        sample_line(
-          Line::new(start, end),
-          LineType::Wooble(5000, 0f64, 0.005 * w_h as f64),
+        (start, end)
+      })
+      .map(|(start, end)| {
+        Line::new(
+          sample_coords(
+            start,
+            CoordType::Slant(0.01 * w_w as f64, 0.01 * w_h as f64),
+          ),
+          sample_coords(end, CoordType::Slant(0.01 * w_w as f64, 0.01 * w_h as f64)),
         )
       })
+      .map(|line| sample_line(line.into(), LineType::Straight(20)))
+      .map(|line| sample_line(line, LineType::Wooble(0f64, 0.004 * w_h as f64)))
+      .map(|line| sample_line(line, LineType::Smooth(3)))
+      .map(|line| sample_line(line, LineType::Straight(50)))
+      .map(|line| sample_line(line, LineType::Wooble(0f64, 0.0025 * w_h as f64)))
       .collect();
 
-    lines.iter().for_each(|line| {
-      line.coords().for_each(|coord| {
+    for line in lines {
+      for coord in line.coords() {
         draw
           .ellipse()
           .x_y(coord.x as f32, coord.y as f32)
-          .w_h(3f32, 3f32)
+          .w_h(2f32, 2f32)
           .color(BLACK);
-      })
-    });
+      }
+    }
   }
 }
--- utils/src/geometry/line.rs
+++ utils/src/geometry/line.rs
@@ -1,50 +1,41 @@
-use geo::{Coord, Line, LineString};
+use geo::{ChaikinSmoothing, LineString};
 
 use super::{sample_coords, CoordType};
 
 pub enum LineType {
   Straight(usize),
-  Wooble(usize, f64, f64),
-  SmoothWooble(usize, f64, f64, usize),
+  Wooble(f64, f64),
+  Smooth(usize),
 }
 
-pub fn sample_line(line: Line, line_type: LineType) -> LineString {
+pub fn sample_line(line: LineString, line_type: LineType) -> LineString {
   match line_type {
-    LineType::Straight(n_sample) => sample_straight_line(line, n_sample),
-    LineType::Wooble(n_sample, std_dev_x, std_dev_y) => {
-      sample_wooble_line(line, n_sample, std_dev_x, std_dev_y)
-    }
-    LineType::SmoothWooble(n_sample, std_dev_x, std_dev_y, window) => {
-      sample_smooth_wooble_line(line, n_sample, std_dev_x, std_dev_y, window)
-    }
+    LineType::Straight(n_sample) => sample_straight(line, n_sample),
+    LineType::Wooble(std_dev_x, std_dev_y) => sample_wooble(line, std_dev_x, std_dev_y),
+    LineType::Smooth(n_iterations) => sample_smooth(line, n_iterations),
   }
 }
 
-fn sample_straight_line(line: Line, n_sample: usize) -> LineString {
-  let sample_coord = line.delta() / (n_sample as f64);
-  (0..n_sample)
-    .map(|i| line.start + sample_coord * (i as f64))
-    .collect::<Vec<Coord>>()
-    .into()
+fn sample_straight(line_string: LineString, n_samples: usize) -> LineString {
+  line_string
+    .lines()
+    .flat_map(|line| {
+      let sample_coord = line.delta() / (n_samples as f64);
+      (0..=n_samples).map(move |i| line.start + sample_coord * (i as f64))
+    })
+    .collect()
 }
 
-fn sample_wooble_line(line: Line, n_sample: usize, std_dev_x: f64, std_dev_y: f64) -> LineString {
+fn sample_wooble(line_string: LineString, std_dev_x: f64, std_dev_y: f64) -> LineString {
   // TODO: unique std_dev and orthogonal sample to line slope
   // Line not wooble enough, use less sample and bigger std_dev for slope
   // the samplke with more points and smaller std_dev
-  sample_straight_line(line, n_sample)
+  line_string
     .into_iter()
     .map(|coord| sample_coords(coord, CoordType::Slant(std_dev_x, std_dev_y)))
-    .collect::<Vec<Coord>>()
-    .into()
+    .collect()
 }
 
-fn sample_smooth_wooble_line(
-  line: Line,
-  n_sample: usize,
-  std_dev_x: f64,
-  std_dev_y: f64,
-  window: usize,
-) -> LineString {
-  line.into()
+fn sample_smooth(line_string: LineString, n_iterations: usize) -> LineString {
+  line_string.chaikin_smoothing(n_iterations)
 }
