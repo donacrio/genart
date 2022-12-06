17f3e94c8a0be023729ea1c278b25186a4ca9107
Donatien Criaud <donatien.criaud@gmail.com>
2022-12-06 14:05:11 +0100

    feat(utils): create dynamic model utilities

---
+++ utils/src/dynamic_artwork/mod.rs
@@ -0,0 +1,6 @@
+mod artwork;
+mod base_model;
+mod constants;
+
+pub use artwork::{DynamicArtwork, DynamicArtworkOptions};
+pub use base_model::{make_dynamic_nannou_app, DynamicBaseModel};
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod brush;
+pub mod dynamic_artwork;
 pub mod geometry;
 pub mod static_artwork;
