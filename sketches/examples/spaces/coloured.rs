4acdf3375df3622884135b0342d5d2f4b903ed98
Donacrio <30463908+donacrio@users.noreply.github.com>
2022-12-27 12:36:29 +0100

    feat(utils): create space division algorithm (#16)
    * feat(utils): create space division algorithm

---
+++ sketches/examples/spaces/coloured.rs
@@ -0,0 +1,116 @@
+use std::path::PathBuf;
+
+use geo::{Coord, Rect};
+use nannou::{
+  prelude::{Key, BLACK, WHITE},
+  App,
+};
+use utils::{
+  algorithms::space::SpaceTile,
+  static_artwork::{make_static_nannou_app, StaticArtwork, StaticArtworkOptions, StaticBaseModel},
+};
+
+const MIN_SIZE: f64 = 100.;
+
+fn main() {
+  make_static_nannou_app::<Model>().run();
+}
+
+struct Tile {
+  rect: Rect,
+}
+
+impl SpaceTile for Tile {
+  fn new(min: Coord, max: Coord) -> Self {
+    Tile {
+      rect: Rect::new(min, max),
+    }
+  }
+
+  fn width(&self) -> f64 {
+    self.rect.width()
+  }
+
+  fn height(&self) -> f64 {
+    self.rect.height()
+  }
+
+  fn min(&self) -> Coord {
+    self.rect.min()
+  }
+
+  fn max(&self) -> Coord {
+    self.rect.max()
+  }
+}
+
+struct Model {
+  base_model: StaticBaseModel,
+  depth: u32,
+  density: f64,
+}
+
+impl StaticArtwork for Model {
+  fn new(base_model: StaticBaseModel) -> Self {
+    Self {
+      base_model,
+      depth: 1,
+      density: 0.5,
+    }
+  }
+  fn get_options() -> StaticArtworkOptions {
+    StaticArtworkOptions {
+      background_path: Some(PathBuf::from("paper.jpg")),
+      ..StaticArtworkOptions::default()
+    }
+  }
+  fn get_model(&self) -> &StaticBaseModel {
+    &self.base_model
+  }
+  fn get_model_mut(&mut self) -> &mut StaticBaseModel {
+    &mut self.base_model
+  }
+  fn current_frame_name(&self) -> String {
+    String::from("frame")
+  }
+  fn draw(&self) {
+    let draw = &self.base_model.draw;
+
+    draw.background().color(WHITE);
+    let [w_w, w_h] = self.base_model.texture.size();
+
+    let w = w_w as f64 * 0.9;
+    let h = w_h as f64 * 0.9;
+    let min: Coord = (-w / 2.0, -h / 2.0).into();
+    let max: Coord = (w / 2.0, h / 2.0).into();
+    let root = Tile::new(min, max);
+
+    let max_children = 2u32.pow(self.depth);
+    let mut space = utils::algorithms::space::compute_space(root, max_children, MIN_SIZE);
+    let leafs = space.leafs();
+    leafs.iter().for_each(|index| {
+      let tile = space.get_node(*index).unwrap().content();
+      let adjusted_rect = Rect::new(
+        tile.rect.min() + (10.0, 10.0).into(),
+        tile.rect.max() - (10.0, 10.0).into(),
+      );
+      utils::paint::fill_rectangle(adjusted_rect, self.density).for_each(|p| {
+        draw
+          .ellipse()
+          .x_y(p.x() as f32, p.y() as f32)
+          .w_h(1.0, 1.0)
+          .color(BLACK);
+      });
+    });
+  }
+
+  fn key_pressed(&mut self, _app: &App, key: Key) {
+    match key {
+      Key::Up => self.depth += 1,
+      Key::Down => self.depth -= 1,
+      Key::Left => self.density -= 0.1,
+      Key::Right => self.density += 0.1,
+      _ => {}
+    }
+  }
+}
---
+++ utils/src/algorithms/space.rs
@@ -0,0 +1,77 @@
+use geo::Coord;
+use rand::Rng;
+use rand_distr::{Bernoulli, Distribution};
+use std::collections::VecDeque;
+
+use crate::data_structures::graph::{Graph, Node};
+
+pub trait SpaceTile {
+  fn new(min: Coord, max: Coord) -> Self;
+  fn width(&self) -> f64;
+  fn height(&self) -> f64;
+  fn min(&self) -> Coord;
+  fn max(&self) -> Coord;
+}
+
+pub type Space<T> = Graph<T>;
+
+pub fn compute_space<T: SpaceTile>(root: T, max_children: u32, min_size: f64) -> Space<T> {
+  let mut space = Space::default();
+  let root = Node::new(root);
+  split_bfs(&mut space, root, max_children, min_size);
+  space
+}
+
+fn split_bfs<T: SpaceTile>(space: &mut Space<T>, root: Node<T>, max_children: u32, min_size: f64) {
+  let parent_index = space.add_node(root);
+
+  let mut queue = VecDeque::new();
+  queue.push_back(parent_index);
+
+  while let Some(index) = queue.pop_front() {
+    // Stop condition on spaces lenght
+    if space.len() >= max_children as usize {
+      return;
+    }
+    let tile = space
+      .get_node(index)
+      .expect("Node with index  does not exists")
+      .content();
+
+    // Only divide rectangle if it's not too small
+    if tile.width() > min_size && tile.height() > min_size {
+      let (child_1, child_2) = divide(tile);
+
+      let child_index_1 = space.add_node(Node::new(child_1));
+      let child_index_2 = space.add_node(Node::new(child_2));
+      space.add_edge(index, child_index_1);
+      space.add_edge(index, child_index_2);
+
+      queue.push_back(child_index_1);
+      queue.push_back(child_index_2);
+    }
+  }
+}
+
+fn divide<T: SpaceTile>(tile: &T) -> (T, T) {
+  let axis = Bernoulli::new(0.5).unwrap().sample(&mut rand::thread_rng());
+
+  match axis {
+    true => {
+      let y = rand::thread_rng().gen_range(0.0..tile.height());
+      let min_1 = tile.min();
+      let max_1 = (tile.max().x, tile.min().y + y).into();
+      let min_2 = (tile.min().x, tile.min().y + y).into();
+      let max_2 = tile.max();
+      (T::new(min_1, max_1), T::new(min_2, max_2))
+    }
+    false => {
+      let x = rand::thread_rng().gen_range(0.0..tile.width());
+      let min_1 = tile.min();
+      let max_1 = (tile.min().x + x, tile.max().y).into();
+      let min_2 = (tile.min().x + x, tile.min().y).into();
+      let max_2 = tile.max();
+      (T::new(min_1, max_1), T::new(min_2, max_2))
+    }
+  }
+}
