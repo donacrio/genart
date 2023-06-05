9380f38bf98a93db74dcaaca44c79038e06676e9
Donatien Criaud <donatien.criaud@gmail.com>
2023-06-05 23:59:22 +0200

    refacto(plants): migrate to nalgebra

--- plants/src/turtle/polygon.rs
+++ plants/src/turtle/polygon.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use geo::{coord, LineString, MultiPolygon, Polygon};
+use nalgebra::{Point3, Rotation3, Vector3};
 
 pub trait TurtleInterpretation {
   fn to_turtle(&self) -> Turtle;
@@ -27,34 +27,45 @@ impl Params {
   }
 }
 
-pub fn to_geom<T: TurtleInterpretation>(commands: Vec<T>, params: &Params) -> MultiPolygon {
+pub fn to_geom<T: TurtleInterpretation>(
+  commands: Vec<T>,
+  params: &Params,
+) -> Vec<Vec<Point3<f64>>> {
   let mut polygons = vec![];
 
-  let mut position = coord! {x:0.0, y:0.0};
-  let mut angle: f64 = 0.0;
+  let mut position = Vector3::new(0.0, 0.0, 0.0);
+  let mut rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), 0.0);
   let mut states = VecDeque::new();
   let mut points = vec![];
   let mut saved_points = VecDeque::new();
   for command in commands.iter() {
     match command.to_turtle() {
-      Turtle::Vertex => points.push(position),
-      Turtle::Forward(length) => {
-        position = position + coord! {x:angle.cos(), y: angle.sin()} * length
+      Turtle::Vertex => points.push(position.into()),
+      Turtle::Forward(length) => position += rotation * Vector3::identity() * length,
+      Turtle::Left => {
+        rotation = match rotation.axis() {
+          Some(axis) => Rotation3::from_axis_angle(&axis, rotation.angle() + params.angle),
+          None => Rotation3::from_axis_angle(&Vector3::z_axis(), params.angle),
+        }
       }
-      Turtle::Left => angle += params.angle,
-      Turtle::Right => angle -= params.angle,
-      Turtle::Push => states.push_back((position, angle)),
-      Turtle::Pop => (position, angle) = states.pop_back().unwrap(),
+      Turtle::Right => {
+        rotation = match rotation.axis() {
+          Some(axis) => Rotation3::from_axis_angle(&axis, rotation.angle() - params.angle),
+          None => Rotation3::from_axis_angle(&Vector3::z_axis(), -params.angle),
+        }
+      }
+      Turtle::Push => states.push_back((position, rotation)),
+      Turtle::Pop => (position, rotation) = states.pop_back().unwrap(),
       Turtle::NewPolygon => {
         saved_points.push_back(points);
         points = vec![];
       }
       Turtle::ClosePolygon => {
-        polygons.push(Polygon::new(LineString::new(points), vec![]));
+        polygons.push(points);
         points = saved_points.pop_back().unwrap();
       }
       Turtle::None => {}
     }
   }
-  MultiPolygon::from(polygons)
+  polygons
 }
--- plants/src/utils/mod.rs
+++ plants/src/utils/mod.rs
@@ -1,3 +1,2 @@
 pub mod app;
 pub mod draw;
-pub mod layout;
